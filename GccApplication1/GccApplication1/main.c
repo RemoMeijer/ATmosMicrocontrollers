49b69ca235def9f323c1ec0ee2d29af5a36c71da
remomeijer <remcomeijer0210@gmail.com>
2021-02-03 09:39:25 +0100

    init

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2/3/2021 9:37:24 AM
+ * Author : remco
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
