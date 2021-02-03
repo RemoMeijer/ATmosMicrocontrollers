c28d08e0f1594d1070d677efcd2a214a7e3b3660
Conner Linskens <cmh.linskens@student.avans.nl>
2021-02-03 11:12:20 +0100

    Added project

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -1,18 +1,29 @@
-/*
- * GccApplication1.c
- *
- * Created: 2/3/2021 9:37:24 AM
- * Author : remco
- */ 
-
 #include <avr/io.h>
+#include <util/delay.h>
 
-
-int main(void)
+void wait( int ms )
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
 }
 
+int main( void )
+{
+	
+	DDRD = 0b11110111;			// All pins PORTD are set to output 
+	
+	while (1)
+	{
+		while(PINC & 0x01){		
+			PORTD = 0x01;
+			wait(250);
+			PORTD = 0x0;
+			wait(250);
+		}
+		PORTD = 0x0;
+	}
+
+	return 1;
+}
