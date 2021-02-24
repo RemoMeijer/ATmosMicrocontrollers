eb2a74f8be887a90853058d7af46eca7e79d4ff0
Conner Linskens <cmh.linskens@student.avans.nl>
2021-02-24 11:09:50 +0100

    Week 3 B2

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -1,80 +1,159 @@
+#define F_CPU 8e6
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
-typedef struct {
-	unsigned char data;
-	unsigned int delay ;
-} PATTERN_STRUCT;
+#define LCD_E 	3
+#define LCD_RS	2
 
-PATTERN_STRUCT pattern[] = {
-	{0x01, 100}, {0x02, 100}, {0x04, 100}, {0x08, 100}, {0x10, 100}, {0x20, 100},
-	{0x01, 100}, {0x02, 100}, {0x04, 100}, {0x08, 100}, {0x10, 100}, {0x20, 100},
-	{0x01, 100}, {0x02, 100}, {0x04, 100}, {0x08, 100}, {0x10, 100}, {0x20, 100},
-	{0x01, 100}, {0x02, 100}, {0x04, 100}, {0x08, 100}, {0x10, 100}, {0x20, 100}, //Rotates 4 times
-	
-	
-	{0x73, 50}, {0x00, 50}, {0x73, 50}, {0x00, 50},
-	{0x44, 50}, {0x22, 50}, {0x44, 50}, {0x22, 50},
-	
-	{0x00, 100},
-	
-	{0x8F, 100}, {0x00, 100}, {0x8F, 100}, {0x00, 100},{0x8F, 100}, {0x00, 100},
-	
-	
-	{0x00, 0},
-};
+void lcd_strobe_lcd_e(void);
+void init_4bits_mode(void);
+void lcd_write_string(char *str);
+void lcd_write_data(unsigned char byte);
+void lcd_write_cmd(unsigned char byte);
 
-const unsigned char Numers[] = {
-	0b00111111, //0
-	0b00000110, //1
-	0b01011011, //2
-	0b01001111, //3
-	0b01100110, //4
-	0b01101101, //5
-	0b01111101, //6
-	0b00000111, //7
-	0b01111111, //8
-	0b01101111, //9
-	0b01110111, //A
-	0b01111100, //b
-	0b00111001, //C
-	0b01011110, //d
-	0b01111001, //E
-	0b01110001, //F
-};
+void lcd_strobe_lcd_e(void) {
+	PORTC |= (1<<LCD_E);	// E high
+	_delay_ms(1);			// nodig
+	PORTC &= ~(1<<LCD_E);  	// E low
+	_delay_ms(1);			// nodig?
+}
 
+void init_4bits_mode(void) {
+	// PORTC output mode and all low (also E and RS pin)
+	DDRC = 0xFF;
+	PORTC = 0x00;
 
-void wait( int ms )
-{
-	for (int i=0; i<ms; i++)
-	{
-		_delay_ms( 1 );        // library function (max 30 ms at 8MHz)
+	// Step 2 (table 12)
+	PORTC = 0x20;	// function set
+	lcd_strobe_lcd_e();
+
+	// Step 3 (table 12)
+	PORTC = 0x20;   // function set
+	lcd_strobe_lcd_e();
+	PORTC = 0x80;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;   // Display on/off control
+	lcd_strobe_lcd_e();
+	PORTC = 0xF0;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;   // Entry mode set
+	lcd_strobe_lcd_e();
+	PORTC = 0x60;
+	lcd_strobe_lcd_e();
+
+}
+
+void lcd_write_string(char *str) {
+	for(;*str; str++){
+		lcd_write_data(*str);
 	}
 }
 
-void display(int number)
-{
-	if(number > 15){
-		PORTE = Numers[14];
-		} else {
-		PORTE = Numers[number];
-	}
+void lcd_write_data(unsigned char byte) {
+	// First nibble.
+	PORTC = byte;
+	PORTC |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTC |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
 }
 
-
-int main( void )
+void lcd_write_command(unsigned char byte)
 {
-	DDRE = 0b11111111;                    // PORTD all output
-	int i = 0;
+	// First nibble.
+	PORTC = byte;
+	PORTC &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
 
-	while (1)
-	{
-		PORTE = pattern[i].data;
-		wait(pattern[i].delay);
-		i++;
-		if(pattern[i].delay == 0){
-			i = 0;
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTC &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+char bin_to_char(unsigned char bin){
+	char returnChar = '0';
+	if(bin < 0b00010000){
+		switch(bin){
+			case 0b00000001 :
+			returnChar = '1';
+			break;
+			case 0b00000010 :
+			returnChar = '2';
+			break;
+			case 0b00000011 :
+			returnChar = '3';
+			break;
+			case 0b00000100 :
+			returnChar = '4';
+			break;
+			case 0b00000101 :
+			returnChar = '5';
+			break;
+			case 0b00000110 :
+			returnChar = '6';
+			break;
+			case 0b00000111 :
+			returnChar = '7';
+			break;
+			case 0b00001000:
+			returnChar = '8';
+			break;
+			case 0b00001001:
+			returnChar = '9';
+			break;
+			case 0b00001010:
+			returnChar = 'A';
+			break;
+			case 0b00001011:
+			returnChar = 'B';
+			break;
+			case 0b00001100:
+			returnChar = 'C';
+			break;
+			case 0b00001101:
+			returnChar = 'D';
+			break;
+			case 0b00001110:
+			returnChar = 'E';
+			break;
+			case 0b00001111:
+			returnChar = 'F';
+			break;
 		}
 	}
-	return 1;
+	return returnChar;	
 }
+
+int main( void ) {
+	
+	DDRD &= ~(1 << 7);
+	DDRB = 0xFF;
+	TCCR2 = 0b00000111;
+
+	// Init LCD
+	init_4bits_mode();
+	
+	lcd_write_command(0x01);
+	
+	// Loop forever
+	while (1) {	
+		lcd_write_command(0x01);
+
+		
+		lcd_write_data(bin_to_char(TCNT2 >> 4));
+		lcd_write_data(bin_to_char(TCNT2 & 0b1111));
+
+		PORTB = TCNT2;
+		_delay_ms( 250 );
+	}
+	return 1;
+}Q
