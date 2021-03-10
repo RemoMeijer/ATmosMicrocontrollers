d536476a7d91fca18fed5b3b041110137c648fe5
Conner Linskens <cmh.linskens@student.avans.nl>
2021-03-10 10:30:36 +0100

    Week 4 B3

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -1,11 +1,86 @@
 #define F_CPU 8e6
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define LCD_E 	3
+#define LCD_RS	2
 
-// Build in led Arduino on PB7 (pin 13)
+void lcd_strobe_lcd_e(void);
+void init_4bits_mode(void);
+void lcd_write_string(char *str);
+void lcd_write_data(unsigned char byte);
+void lcd_write_cmd(unsigned char byte);
+
+void lcd_strobe_lcd_e(void) {
+	PORTC |= (1<<LCD_E);	// E high
+	_delay_ms(1);			// nodig
+	PORTC &= ~(1<<LCD_E);  	// E low
+	_delay_ms(1);			// nodig?
+}
+
+void init_4bits_mode(void) {
+	// PORTC output mode and all low (also E and RS pin)
+	DDRC = 0xFF;
+	PORTC = 0x00;
+
+	// Step 2 (table 12)
+	PORTC = 0x20;	// function set
+	lcd_strobe_lcd_e();
+
+	// Step 3 (table 12)
+	PORTC = 0x20;   // function set
+	lcd_strobe_lcd_e();
+	PORTC = 0x80;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;   // Display on/off control
+	lcd_strobe_lcd_e();
+	PORTC = 0xF0;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;   // Entry mode set
+	lcd_strobe_lcd_e();
+	PORTC = 0x60;
+	lcd_strobe_lcd_e();
+
+}
+
+void lcd_write_string(char *str) {
+	for(;*str; str++){
+		lcd_write_data(*str);
+	}
+}
+
+void lcd_write_data(unsigned char byte) {
+	// First nibble.
+	PORTC = byte;
+	PORTC |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTC |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+void lcd_write_command(unsigned char byte)
+{
+	// First nibble.
+	PORTC = byte;
+	PORTC &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTC &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
 void wait( int ms ) {
 	for (int i=0; i<ms; i++) {
 		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
@@ -18,17 +93,35 @@ void adcInit( void )
 	ADCSRA = 0b10000110;
 }
 
-int main(void) {
+
+int main( void ) {
+	char *temperature = calloc(1, 25);
+	
 	DDRF = 0x00;
 	DDRA = 0xFF;
 	DDRB = 0xFF;
 	adcInit();
+
+	// Init LCD
+	init_4bits_mode();
 	
-	while (1)  {
+	lcd_write_command(0x01);
+	
+	// Loop forever
+	while (1) {
 		ADCSRA |= (1 << 6);
 		while ( ADCSRA & (1 << 6) ) ;
 		PORTA = ADCH;
+
+		lcd_write_command(0x01);
+		
+		sprintf(temperature, "%d", ADCH);
+		
+		lcd_write_string(temperature);
+
 		wait(100);
 	}
-	return 0;
+	
+	free(temperature);
+	return 1;
 }
