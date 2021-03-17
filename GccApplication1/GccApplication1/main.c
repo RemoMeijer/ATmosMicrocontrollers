7e82d91a5d606652494330c82db6026eef67cf2b
Conner Linskens <cmh.linskens@student.avans.nl>
2021-03-17 10:40:04 +0100

    Able to read multiple ADC values

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -5,6 +5,22 @@
 
 #define F_CPU 8e6
 
+void adcInit( void )
+{
+	ADMUX = 0b01100001;
+	ADCSRA = 0b10000110;
+}
+
+void changeADCChannel(int channel){
+	ADMUX = 0x60 | channel;
+}
+
+int readADC(){
+	ADCSRA |= (1 << 6);
+	while ( ADCSRA & (1 << 6) );
+	return ADCH;
+}
+
 void wait(int ms)
 {
 	for (int i=0; i<ms; i++)
@@ -13,9 +29,19 @@ void wait(int ms)
 
 int main()
 {	
-	while(1){
-		wait(1000);
-	}
+	DDRF = 0x00;
+	DDRA = 0xFF;
+	DDRD = 0xFF;
+	DDRE = 0xFF;
+	
+	adcInit();
 
+	while(1){
+		changeADCChannel(0);
+		PORTD = readADC();
+		changeADCChannel(1);
+		PORTE = readADC();
+		wait(100);
+	}
 	return (1);
 }
