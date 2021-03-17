18e0d86400535f4d970b1289e09fb232b64f6ec2
Conner Linskens <cmh.linskens@student.avans.nl>
2021-03-17 12:17:01 +0100

    Added oscillator timer (doesnt work)

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -1,9 +1,27 @@
 #include <avr/io.h>
+#include <avr/sfr_defs.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include <util/delay.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 #define F_CPU 8e6
+
+int outpitch = 0;
+int wavenum = 0;
+int envval = 1;
+int waveform;
+
+int pitch = 400;
+int lfoval = 0;
+
+static uint16_t waveforms[4] =
+{
+	0b1100000000000000,
+	0b1111111100000000,
+	0b1010101010101010,
+	0b0000000000000000
+};
 
 void adcInit( void )
 {
@@ -27,6 +45,40 @@ void wait(int ms)
 	_delay_ms(1);
 }
 
+ISR(TIMER1_COMPA_vect)
+{
+	// take first bit
+	int shiftout = waveform & 1;
+	// move waveform 1 place to the right
+	waveform >>= 1;
+	
+	// if we removed a 1 with the bit shift put a 1 to the end of the waveform
+	// else do nothing and let it stay 0
+	if(shiftout)
+		waveform |= (1 << 15);
+	
+	//shiftout &= envval;
+	
+	if(shiftout)
+		PORTE |= (1 << 2);
+	else
+		PORTE &= ~(1 << 2);
+	}
+
+// set new pitch
+void update_pitch()
+{
+	uint16_t newpitch = 200U + ((pitch-lfoval)*4U);
+	
+	if (newpitch != outpitch)
+	{
+		outpitch = newpitch;
+		if (TCNT1 > outpitch)
+		TCNT1 = 0;
+		OCR1A = outpitch;
+	}
+}
+
 int main()
 {	
 	DDRF = 0x00;
@@ -35,12 +87,25 @@ int main()
 	DDRE = 0xFF;
 	
 	adcInit();
-
+	
+	sei();
+	
+	TCCR1B |= _BV(WGM12);  // enable CTC
+	TIMSK |= _BV(OCIE1A); // enable CTC interrupt
+	
+	wavenum = 3;
+	waveform = waveforms[wavenum];
+	
+	TCCR1B |= _BV(CS11); // set prescaler to 8; starts oscillator
+	
 	while(1){
 		changeADCChannel(0);
-		PORTD = readADC();
+		PORTA = readADC();
 		changeADCChannel(1);
-		PORTE = readADC();
+		PORTD = readADC();
+		
+		//update_pitch();
+		
 		wait(100);
 	}
 	return (1);
