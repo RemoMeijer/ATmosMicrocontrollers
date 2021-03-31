a94eedaa5140bec2356b31809c32bec917e8fde9
Conner Linskens <cmh.linskens@student.avans.nl>
2021-03-31 09:55:34 +0200

    Added waveforms and random noise

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -7,6 +7,7 @@
 #include <util/delay.h>
 #include <stdlib.h>
 
+#define NUM_WAVEFORMS 6
 
 int outpitch = 0;
 int wavenum = 0;
@@ -16,11 +17,13 @@ int waveform;
 int pitch = 400;
 int lfoval = 0;
 
-static uint16_t waveforms[4] =
+static uint16_t waveforms[NUM_WAVEFORMS] =
 {
-	0b1111011111011110,
-	0b1111111100000000,
 	0b1010101010101010,
+	0b1111111100000000,
+	0b0010100001110110,
+	0b1111011111011110,
+	0b0000000000000000, // random noise
 	0b0000000000000000
 };
 
@@ -46,14 +49,34 @@ void wait(int ms)
 	_delay_ms(1);
 }
 
+void changeWaveform(){
+	wavenum++;
+	wavenum = wavenum > NUM_WAVEFORMS - 1? 0 : wavenum;
+	wavenum = wavenum < 0? NUM_WAVEFORMS - 1 : wavenum;
+	waveform = waveforms[wavenum];
+}
+
+// faster noise generator than rand()
+uint16_t noise()
+{
+	static uint16_t lfsr = 0xACE1u;
+	lfsr = (lfsr >> 1) ^ (-(lfsr & 1u) & 0xB400u);
+	return lfsr;
+}
+
 ISR(TIMER1_COMPA_vect)
 {
-	// take first bit
 	uint8_t shiftout;
-	shiftout = waveform & (1<<0);
-	// move waveform 1 place to the right
-	waveform >>= 1;
-		
+	
+	if(wavenum != NUM_WAVEFORMS - 2){
+		// take first bit
+		shiftout = waveform & (1<<0);
+		// move waveform 1 place to the right
+		waveform >>= 1;
+	} else {
+		shiftout = noise() & 1;
+	}
+	
 	// if we removed a 1 with the bit shift put a 1 to the end of the waveform
 	// else do nothing and let it stay 0
 	waveform &= ~(1 << 15);
@@ -77,7 +100,7 @@ ISR(TIMER1_COMPA_vect)
 // set new pitch
 void update_pitch()
 {
-	uint16_t newpitch = 1U + (floor((pitch-lfoval)*1U)/10);
+	uint16_t newpitch = 200U + ((pitch-lfoval)*4U);
 	
 	if (newpitch != outpitch)
 	{
@@ -93,8 +116,7 @@ void setupTimer(){
 	TIMSK |= (1 << 4);
 	sei();
 	TCCR1A = 0b0;
-	TCCR1B = 0b1100;
-	
+	TCCR1B = 0b1010;
 }
 
 int main()
@@ -103,12 +125,11 @@ int main()
 	DDRA = 0xFF;
 	DDRB = 0xFF;
 	DDRD = 0x0;
-	//DDRD = 0xFF;
 	DDRE = 0b01000110;
 	DDRG = 0x00;
 	adcInit();
 	int pressed = 0;
-	wavenum = 2;
+	wavenum = 0;
 	waveform = waveforms[wavenum];
 	
 	setupTimer();
@@ -123,11 +144,7 @@ int main()
 		
 		if(PINE & 0x80){
 			if(pressed == 0){
-				//PORTE ^= (1<<6);
-				wavenum++;
-				wavenum = wavenum > 3? 0 : wavenum;
-				wavenum = wavenum < 0? 3 : wavenum;
-				waveform = waveforms[wavenum];
+				changeWaveform();
 				pressed = 1;
 			}
 		} else { 
